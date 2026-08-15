bf07b3fa3a67d32853beef52327cff81185f96fb
lilsnibbi <me@lilsnibbi.dev>
2026-08-16 09:46:53 +1000

    updated tag.ts

--- scripts/tag.ts
+++ scripts/tag.ts
@@ -4,10 +4,8 @@ if (!tag || !/^v\d+\.\d+\.\d+(?:-[0-9A-Za-z.-]+)?$/.test(tag)) {
 	throw new Error("Usage: bun scripts/tag.ts v<major>.<minor>.<patch>");
 }
 
-await Bun.$`git fetch origin main`.quiet(true);
-await Bun.$`git tag --force --annotate ${tag} origin/main --message ${tag}`.quiet(
-	true,
-);
-await Bun.$`git push origin refs/tags/${tag}`.quiet(true);
+await Bun.$`git fetch origin main`
+await Bun.$`git tag --force --annotate ${tag} origin/main --message ${tag}`
+await Bun.$`git push origin refs/tags/${tag}`
 
 console.log(`Tag created: ${tag}`);
