323567074224390c6cb6202f68b0d9bfe841bfea
lilsnibbi <me@lilsnibbi.dev>
2026-08-16 09:29:35 +1000

    chore: added tag script

---
+++ scripts/tag.ts
@@ -0,0 +1,6 @@
+const tag = Bun.argv[2];
+
+await Bun.$`git tag -d ${tag}`.quiet(true);
+await Bun.$`git tag -a ${tag} main -m \"${tag}\"`.quiet(true);
+await Bun.$`git push ref/tags/${tag}`.quiet(true);
+console.log(`Tag created: ${tag}`);
